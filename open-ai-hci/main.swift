f0eb2dd662e4f6e2262af91b344558f9551edea2
fraander <57777918+fraander@users.noreply.github.com>
2024-01-27 10:41:51 -0500

    Add MacPaw OpenAI package

--- open-ai-hci/main.swift
+++ open-ai-hci/main.swift
@@ -6,6 +6,12 @@
 //
 
 import Foundation
+import OpenAI
 
-print("Hello, World!")
+let configuration = OpenAI.Configuration(token: "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")
+let openAI = OpenAI(configuration: configuration)
 
+let query = ChatQuery(model: .gpt3_5Turbo, messages: [.init(role: .user, content: "who are you")])
+for try await result in openAI.chatsStream(query: query) {
+    result.
+}
