6aa09ef31d5e07d4f0fd764c3c10ca084351cf64
fraander <57777918+fraander@users.noreply.github.com>
2024-01-27 10:58:07 -0500

    Make API call and display response

--- open-ai-hci/main.swift
+++ open-ai-hci/main.swift
@@ -8,10 +8,16 @@
 import Foundation
 import OpenAI
 
-let configuration = OpenAI.Configuration(token: "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")
-let openAI = OpenAI(configuration: configuration)
+let configuration = OpenAI.Configuration(token: "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") // setup the configuration object with my API key so that I can make calls to the API
+let openAI = OpenAI(configuration: configuration) // setup the OpenAI object to handle API usage, using my previously defined configuration
 
-let query = ChatQuery(model: .gpt3_5Turbo, messages: [.init(role: .user, content: "who are you")])
-for try await result in openAI.chatsStream(query: query) {
-    result.
+let query = ChatQuery(model: .gpt3_5Turbo, messages: [.init(role: .user, content: "Give a creative spin on 'Hello, world!'")]) // build a query with a the GPT 3.5 Turbo model, and ask for a response to the prompt: "Give me a creative spin on 'Hello, world!'"
+
+var result = "" // prepare to accumulate the result
+for try await response in openAI.chatsStream(query: query) {
+    if let r = response.choices.first?.delta.content {
+        result += r
+    }
 }
+
+print("RESULT: \(result)")
