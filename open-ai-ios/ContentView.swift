e4d260c998f2d16b695ff2ed437d3f102f0e4e2b
fraander <57777918+fraander@users.noreply.github.com>
2024-01-27 11:39:39 -0500

    Add more documentation notes

--- open-ai-ios/ContentView.swift
+++ open-ai-ios/ContentView.swift
@@ -10,49 +10,55 @@ import SwiftUI
 struct ContentView: View {
     
     @EnvironmentObject var openAIService: OpenAIService // retrieve the API service from the environment
-    @State var helloWorld = ""
+    @State var helloWorld = "" // hold onto the text to display, use @State since it will change and you want the view to update to reflect this
     
     var body: some View {
-        VStack(spacing: 24) {
+        VStack(spacing: 24) { // display content in this container stacked vertically
             
-            Spacer()
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.white)
+            Spacer() // push content down
+            
+            Image(systemName: "globe") // Show a Globe using built-in SF Symbols
+                .imageScale(.large) // make it larger
+                .foregroundStyle(.white) // make it white
+            
             Text(helloWorld)
-                .multilineTextAlignment(.center)
-                .font(.system(.title3, design: .monospaced, weight: .bold))
-                .foregroundStyle(.white)
+                .multilineTextAlignment(.center) // center the text
+                .font(.system(.title3, design: .monospaced, weight: .bold)) // set the font to monospaced, bold, and slightly larger than standard
+                .foregroundStyle(.white) // make the text white
             
-            Spacer()
+            Spacer() // push content up and down
             
-            Button("Reload", systemImage: "arrow.counterclockwise") {
-                Task {
-                    await reload()
+            Button("Reload", systemImage: "arrow.counterclockwise") { // Reload Button
+                Task { // create an asynchronous processes
+                    await reload() // run the reload function
                 }
             }
-            .buttonStyle(.bordered)
+            .buttonStyle(.bordered) // style the button using a built-in style
             .tint(.white)
         }
-        .padding(24)
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-        .background {
+        .padding(24) // add 24 pts of space around each edge
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center) // expand to fill teh container
+        .background { // make a gradient background going mint -> teal -> indigo
             LinearGradient(gradient: Gradient(colors: [.mint, .teal, .indigo]), startPoint: .top, endPoint: .bottom)
         }
-        .ignoresSafeArea(.all)
-        .task { await reload() }
+        .ignoresSafeArea(.all) // fit the whole screen
+        .task { await reload() } // on first load, run reload function
     }
     
-    func reload() async {
+    
+    func reload() async { // reload function is asynchronous and doesn't return any value
+        // ask the service to create a new version of 'hello, world'.
         if let value = try? await openAIService.spinHelloWorld() {
+            // if successful, update the value to display to the new value
             helloWorld = value
         }
     }
 }
 
+// support a preview by replicating how this component is normally displayed
 #Preview {
 
     @StateObject var openAIService: OpenAIService = .init() // create the API Service wrapper to handle results
 
-    return ContentView().environmentObject(openAIService)
+    return ContentView().environmentObject(openAIService) // inject the service into the container
 }
--- open-ai-ios/OpenAIService.swift
+++ open-ai-ios/OpenAIService.swift
@@ -8,19 +8,23 @@
 import SwiftUI
 import OpenAI
 
+
+// Hold onto the different API functions and the configuration
 class OpenAIService: ObservableObject {
     let openAI = OpenAI(configuration: .init(token: "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")) // setup the OpenAI object to handle API usage, using my API token to configure
 
+    // Query the API to give a new version of hello world. This function is asynchronous and can throw an error
     func spinHelloWorld() async throws -> String {
         let query = ChatQuery(model: .gpt3_5Turbo, messages: [.init(role: .user, content: "Give a creative spin on 'Hello, world!'")]) // build a query with a the GPT 3.5 Turbo model, and ask for a response to the prompt: "Give me a creative spin on 'Hello, world!'"
 
         var result = "" // prepare to accumulate the result
         for try await response in openAI.chatsStream(query: query) {
+            // if there is a response, add it to the stream of characters in `result`
             if let r = response.choices.first?.delta.content {
-                result += r
+                result += r // add to `result`
             }
         }
 
-        return result
+        return result // return finished `result`
     }
 }
